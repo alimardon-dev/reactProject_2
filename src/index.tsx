f9247468b7be30fc4a26ed547f302d06c7b84c72
Alimardon <asotvoldiev922@gmail.com>
2025-02-02 08:43:34 +0900

    modified after

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// location
-var location = document.getElementById("root");
-
-// time module
-var time = new Date(2025, 12, 12, 13);
-
-// current time
+var time = new Date();
 var hour = time.getHours();
 
-var greeting1 = "Good Morning";
-var greeting2 = "Good Afternoon";
-var greeting3 = "Good Night";
+var heading = {
+  color: "",
+};
 
-var greeting = "";
+var greeting;
 if (hour < 12) {
-  greeting = greeting1;
+  greeting = "Good Morning";
+  heading.color = "red";
 } else if (hour >= 12 && hour < 18) {
-  greeting = greeting2;
+  greeting = "Good Afternoon";
+  heading.color = "green";
 } else {
-  greeting = greeting3;
-}
-var heading;
-
-if (greeting == greeting1) {
-  heading = {
-    color: "red",
-  };
-} else if (greeting == greeting2) {
-  heading = {
-    color: "green",
-  };
-} else {
-  heading = {
-    color: "blue",
-  };
+  greeting = "Good Night";
+  heading.color = "blue";
 }
 
-// code
-var code = (
-  <div>
-    <h1 style={heading}>{greeting}</h1>
-  </div>
-);
-
-ReactDOM.render(code, location);
+ReactDOM.render(<h1 style={heading}>{greeting}</h1>, document.getElementById("root"));
